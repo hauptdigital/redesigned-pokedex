c53c21436c84a813c8d8e2e757dd2a30bd67052f
Marc <marc@haupt.digital>
2020-03-08 14:03:27 +0100

    Search functionality (#5)
    * Add searchwrapper and searchinput
    
    * Reduce API to fetch name, id and image path
    
    * Add pokemon filter functionality and output in console
    
    * Add pokemon filter functionality and output in search result wrapper
    
    * Upload pokemon pictures in image folder
    
    * Replace forEach with filter function

--- src/app.js
+++ src/app.js
@@ -6,23 +6,23 @@ export function app() {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=5');
     const results = await response.json();
     const pokemons = await results.results;
+
     const pokemonData = [];
 
-    pokemons.forEach(async pokemon => {
-      const response = await fetchPokemon(pokemon);
-      pokemonData.push(response);
-      const pokemonDiv = document.createElement('div');
-      pokemonDiv.innerText = pokemonData.name;
-    });
-    console.log(pokemonData);
-    return pokemonData;
-  }
+    pokemons.forEach(pokemon => {
+      // Sorry for the dirty implementation of pictures.
 
-  async function fetchPokemon(pokemon) {
-    let url = pokemon.url;
-    const response = await fetch(url);
-    const results = await response.json();
-    const pokemonData = results;
+      const subStrings = pokemon.url.split('/');
+      const pokemonId = subStrings[6];
+      const imagePath =
+        'https://pokeres.bastionbot.org/images/pokemon/' + pokemonId + '.png';
+      pokemonData.push({
+        name: pokemon.name,
+        id: pokemonId,
+        imagePath: imagePath
+      });
+    });
+
     return pokemonData;
   }
 
@@ -39,18 +39,82 @@ export function app() {
     innerText: 'Pokédex'
   });
 
+  // Search
+
+  const searchWrapper = createElement('div', {
+    className: 'searchWrapper'
+  });
+  const search = createElement('input', {
+    className: 'searchWrapper__input',
+    type: 'text'
+  });
+
+  // Search Results
+
+  const searchResultsWrapper = createElement('div', {
+    className: 'searchResultsWrapper'
+  });
+
+  async function createSearchResults() {
+    search.addEventListener('input', async event => {
+      const input = event.target;
+      const searchQuery = input.value;
+
+      // Get Data
+      const pokemonData = await fetchPokemons();
+
+      // Filter results
+      const filteredPokemons = filterResults(searchQuery, pokemonData);
+
+      // Create search results
+      removeAllChilds(searchResultsWrapper);
+
+      filteredPokemons.forEach(pokemon => {
+        const searchResult = createElement('div', {
+          className: 'searchResultsWrapper__searchResult'
+        });
+        searchResult.dataset.pokemonid = pokemon.id;
+        const searchResultTitle = createElement('h2', {
+          className: 'searchResultsWrapper__title',
+          innerText: pokemon.name
+        });
+        const searchResultImageWrapper = createElement('div', {
+          className: 'searchResultsWrapper__imageWrapper'
+        });
+        const searchResultImage = createElement('img', {
+          className: 'searchResultsWrapper__image',
+          src: pokemon.imagePath
+        });
+        appendElement(searchResultImageWrapper, [searchResultImage]);
+        appendElement(searchResult, [
+          searchResultTitle,
+          searchResultImageWrapper
+        ]);
+        appendElement(searchResultsWrapper, [searchResult]);
+      });
+    });
+
+    function filterResults(searchQuery, pokemonData) {
+      // Get Pokemon names
+
+      const filteredPokemons = pokemonData.filter(pokemon => {
+        const pokemonName = pokemon.name;
+        return pokemonName.toLowerCase().startsWith(searchQuery.toLowerCase());
+      });
+
+      return filteredPokemons;
+    }
+  }
+
+  createSearchResults();
+
   // Main
   const main = createElement('main', { className: 'main' });
   // Build app
 
   appendElement(header, [logo, title]);
-  //   appendElement(main, [
-  //     searchElement,
-  //     searchResultsWrapper,
-  //     favoritesSection
-  //   ]);
-
-  fetchPokemons();
+  appendElement(searchWrapper, [search]);
+  appendElement(main, [searchWrapper, searchResultsWrapper]);
 
   return [header, main];
 }
